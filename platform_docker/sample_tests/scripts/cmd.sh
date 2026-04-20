62b4ebb4d757ece0f66e51f79e64e5894717154f
Rjochi <f22hakuti@gmail.com>
2026-04-21 06:30:15 +0900

    [UPD] Add ROS SMACH libraries to Dockerfile and update cmd.sh for launch sequence

--- platform_docker/sample_tests/scripts/cmd.sh
+++ platform_docker/sample_tests/scripts/cmd.sh
@@ -5,6 +5,22 @@ export ROS_IP=`hostname -I | cut -d ' ' -f1`
 source /root/catkin_ws/devel/setup.bash
 
 # select the launch file
-# /opt/ros/noetic/bin/roslaunch intball_programs iss_static_map_server.launch
+# /opt/ros/noetic/bin/rosrun intball_programs run_competition.py
+
+
+echo "Starting run_competition.py..."
+/opt/ros/noetic/bin/rosrun intball_programs run_competition.py &
+
+# 4秒待機
+sleep 4
+
+# 2. 1つ目のlaunchをバックグラウンドで起動
+echo "Starting first launch..."
+/opt/ros/noetic/bin/roslaunch intball_programs gnc.launch &
+
+# 10秒待機
+sleep 10
+
+# 3. 別のlaunchを起動 (最後なので & はあってもなくてもOK)
+echo "Starting second launch..."
 /opt/ros/noetic/bin/roslaunch intball_programs yolo.launch
-# /opt/ros/noetic/bin/roslaunch sample_tests test_fan_duty.launch
